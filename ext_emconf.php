001bff6ec02ccebde47e9ca6c62edc9931e82c09
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-05-11 11:54:46 +0200

    initial

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+/*****************************************************************************
+ * Extension Manager/Repository config file for ext "threesixty_viewer". *
+ ****************************************************************************/
+$EM_CONF['threesixty_viewer'] = array(
+	'title' => '360° viewer',
+	'description' => 'Plugin to display 360° images',
+	'category' => 'fe',
+	'state' => 'stable',
+	'author' => 'Sebastian Michaelsen',
+	'author_email' => 'sebastian@app-zap.de',
+	'author_company' => 'app zap',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.99.99',
+		),
+	),
+);
